c1cc92dc58e6c946d81eca3a5882e2e88f05c61c
Benefit <101987140+benefit6@users.noreply.github.com>
2024-06-22 22:48:31 +0800

    Update diy.sh

--- devices/amlogic_meson8b/diy.sh
+++ devices/amlogic_meson8b/diy.sh
@@ -8,7 +8,11 @@ bash $SHELL_FOLDER/../common/kernel_6.1.sh
 
 git_clone_path master https://github.com/coolsnowwolf/lede target/linux/amlogic package/boot/uboot-amlogic
 
+sed -i 's/+uci-firewall/+firewall/g' feeds/luci/applications/luci-app-firewall/Makefile
+sed -i "s/192.168.1.1/192.168.3.10/" package/base-files/files/bin/config_generate
 sed -i "s/wpad-openssl/wpad-basic-mbedtls/" target/linux/amlogic/image/meson8b.mk
+sed -i "s/%D %V %C/%D $(TZ=UTC-8 date +%Y.%m.%d)/" package/base-files/files/etc/openwrt_release
+sed -i "s/%R/by Benefit66/" package/base-files/files/etc/openwrt_release
 
 
 
